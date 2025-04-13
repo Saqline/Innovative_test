8bcd3f98764df9c7b53a2d0df9495ed78de5d739
Imran Nur Tanha <82803676+Saqline@users.noreply.github.com>
2025-04-13 12:01:35 +0000

    Backend System Push with sqlite db

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+# Initializing the database connection with sqlite
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
