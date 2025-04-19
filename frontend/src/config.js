474e3b180631064d6e917509c3b50f700cef6246
amran <imrannurint@gmail.com>
2025-04-19 16:07:31 +0600

    deploy

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+// export const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
 
-// export const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://innovative-test.onrender.com';
+export const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://innovative-test.onrender.com';
